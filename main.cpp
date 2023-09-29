d4b2051a35dc9b6baf6c182f19816f7a81db3692
fchzo08 <fachrulghifari04@gmail.com>
2023-09-30 05:22:20 +0700

    Kurang SUPER

--- main.cpp
+++ main.cpp
@@ -77,15 +77,168 @@ void encryptXOR(){
 	std::cout<<"==========\nDecrypt = "<<decryptXOR(builder.str(),key)<<std::endl;
 }
 
+std::string decryptRailFence(std::string cipher, int kunci)
+{
+    // Membuat matriks untuk mendekripsi pesan
+    // kunci = baris, panjang(cipher) = kolom
+    char rail[kunci][cipher.length()];
+
+    // Mengisi matriks rail untuk membedakan antara karakter terisi dan karakter kosong
+    for (int i = 0; i < kunci; i++)
+        for (int j = 0; j < cipher.length(); j++)
+            rail[i][j] = '\n';
+
+    // Untuk menentukan arah aliran
+    bool arah_ke_bawah;
+
+    int baris = 0, kolom = 0;
+
+    // Menandai tempat-tempat dengan '*'
+    for (int i = 0; i < cipher.length(); i++)
+    {
+        // Memeriksa arah aliran
+        if (baris == 0)
+            arah_ke_bawah = true;
+        if (baris == kunci - 1)
+            arah_ke_bawah = false;
+
+        // Menempatkan penanda
+        rail[baris][kolom++] = '*';
+
+        // Mencari baris berikutnya menggunakan tanda arah
+        arah_ke_bawah ? baris++ : baris--;
+    }
+
+    // Sekarang kita dapat mengonstruksi matriks rail
+    int indeks = 0;
+    for (int i = 0; i < kunci; i++)
+        for (int j = 0; j < cipher.length(); j++)
+            if (rail[i][j] == '*' && indeks < cipher.length())
+                rail[i][j] = cipher[indeks++];
+
+    // membaca matriks dalam pola zig-zag untuk mengonstruksi
+    // teks hasil dekripsi
+    std::string hasil;
+
+    baris = 0, kolom = 0;
+    for (int i = 0; i < cipher.length(); i++)
+    {
+        // Memeriksa arah aliran
+        if (baris == 0)
+            arah_ke_bawah = true;
+        if (baris == kunci - 1)
+            arah_ke_bawah = false;
+
+        // Menempatkan penanda
+        if (rail[baris][kolom] != '*')
+            hasil.push_back(rail[baris][kolom++]);
+
+        // Mencari baris berikutnya menggunakan tanda arah
+        arah_ke_bawah ? baris++ : baris--;
+    }
+    return hasil;
+}
+
+void encryptRailFence(){
+	system("cls");
+	std::string teks; 
+	int kunci;
+	std::cout<<"Rail Fence ENCRYPT\n===============================\n";
+	std::cout<<"Masukan Teks : ";std::getline(std::cin,teks);
+	std::cout<<"Masukan Key : ";std::cin>>kunci;
+    // Membuat matriks untuk mengenkripsi pesan
+    // kunci = baris, panjang(teks) = kolom
+    char rail[kunci][(teks.length())];
+
+    // Mengisi matriks rail untuk membedakan antara karakter terisi dan karakter kosong
+    for (int i = 0; i < kunci; i++)
+        for (int j = 0; j < teks.length(); j++)
+            rail[i][j] = '\n';
+
+    // Untuk menentukan arah aliran
+    bool arah_ke_bawah = false;
+    int baris = 0, kolom = 0;
+
+    for (int i = 0; i < teks.length(); i++)
+    {
+        // Memeriksa arah aliran
+        // Membalik arah jika kita baru saja mengisi baris atas atau bawah
+        if (baris == 0 || baris == kunci - 1)
+            arah_ke_bawah = !arah_ke_bawah;
+
+        // Mengisi huruf yang sesuai
+        rail[baris][kolom++] = teks[i];
+
+        // Mencari baris berikutnya menggunakan tanda arah
+        arah_ke_bawah ? baris++ : baris--;
+    }
+
+    // Sekarang kita dapat mengonstruksi cipher menggunakan matriks rail
+   std::string hasil;
+    for (int i = 0; i < kunci; i++)
+        for (int j = 0; j < teks.length(); j++)
+            if (rail[i][j] != '\n')
+                hasil.push_back(rail[i][j]);
+
+    std::cout<<"\nAfter Modified\n";
+	std::cout<<"Plain = "<<teks<<std::endl;
+	std::cout<<"Key = "<<kunci<<std::endl;
+	std::cout<<"Result Encrypt = "<<hasil<<std::endl;
+	std::cout<<"==========\nDecrypt = "<<decryptRailFence(hasil,kunci)<<std::endl;
+}
+
+
+std::string encryptCaesarCipher(std::string teks, int kunci) {
+    std::string hasil = "";
+    for (int i = 0; i < teks.length(); i++) {
+        // Periksa apakah karakter adalah huruf
+        if (isalpha(teks[i])) {
+            char shift = isupper(teks[i]) ? 'A' : 'a';
+            hasil += char(((teks[i] + kunci - shift) % 26) + shift);
+        } else {
+            // Jika bukan huruf, tambahkan karakter aslinya ke hasil
+            hasil += teks[i];
+        }
+    }
+    return hasil;
+}
+
+// Fungsi untuk mendekripsi pesan dengan Caesar Cipher
+std::string decryptCaesarCipher(std::string teks, int kunci) {
+    return encryptCaesarCipher(teks, 26 - kunci);
+}
+
+void caesarCipher() {
+	system("cls");
+    std::string teks,d;
+    int kunci;
+    std::cout<<"Caesar CipherENCRYPT\n===============================\n";
+	std::cout<<"Masukan Teks : ";std::getline(std::cin,teks);
+	std::cout<<"Masukan Key : ";std::cin>>kunci;
+	std::cout<<"\nAfter Modified\n";
+	std::cout<<"Plain = "<<teks<<std::endl;
+	std::cout<<"Key = "<<kunci<<std::endl;
+	std::cout<<"Result Encrypt = "<<encryptCaesarCipher(teks,kunci)<<std::endl;
+	d = encryptCaesarCipher(teks,kunci);
+	std::cout<<"==========\nDecrypt = "<<decryptCaesarCipher(d,kunci)<<std::endl;
+}
+
 int main(int argc, char** argv) {
 	int a=0;
-	std::cout<<"Pilih Mode Enkripsi\n1.XOR\n2.CAESAR CHIPPER\n3.\n4.SUPERENCRYPT"<<std::endl;
+	std::cout<<"Pilih Mode Enkripsi\n1.XOR\n2.CAESAR CHIPPER\n3.Rail Fence\n4.SUPERENCRYPT"<<std::endl;
 	std::cin>>a;std::cin.ignore();
 	switch(a){
 		case 1:
 			encryptXOR();
 			break;
+		case 2:
+			caesarCipher();
+			break;
+		case 3:
+			encryptRailFence();
+			break;
 		default:
 			break;
 	}
 }
+
